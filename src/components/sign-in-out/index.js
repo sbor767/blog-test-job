d29e009b200e23bfbf809485e51a64637238c21f
Alersandr Borkov <sbor767@yandex.ru>
2018-10-28 14:33:32 +0300

    Fix SignInOut component for signOut callback.

--- src/components/sign-in-out/index.js
+++ src/components/sign-in-out/index.js
@@ -7,18 +7,22 @@ import Button from '../button'
 import ButtonTo from '../button-to'
 
 function SignInOut({ children, user, dispatch }) {
+
+  const signOut = () => actions.user.signOut()(dispatch)
+
   return (
     <div className="SignInOut">
     {!!user.id ? (
       <div className="SignInOut_wrap">
         <div className="SignInOut_buttons">
           {children}
-          <Button title={'SignOut'} onClick={actions.user.signOut()(dispatch)} classes={['blue']}/>
+          <Button title={'SignOut'} onClick={signOut} classes={['blue']}/>
         </div>
         <div className="SignInOut_welcome"><p>Welcome <span className="SignInOut_welcome_name">{user.name}</span></p></div>
       </div>
     ) : (
       <div className="SignInOut_wrap">
+        {/* @TODO Add history  */}
         <ButtonTo title={'SignIn'} to={'/sign-in'} classes={['blue', 'ButtonTo_float_right', 'clearfix']}/>
         <div className="SignInOut_welcome"><p>Sign in to be <span className="SignInOut_welcome_name">right guy</span></p></div>
       </div>
