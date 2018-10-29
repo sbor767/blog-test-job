f1a383b2080c52ff76a41725a91b391cfb4fc818
Alersandr Borkov <sbor767@yandex.ru>
2018-10-29 13:07:36 +0300

    Rate component implementation. Dirty.

--- src/containers/post-page/comment/index.js
+++ src/containers/post-page/comment/index.js
@@ -9,6 +9,16 @@ import Copyright from '../../../components/copyright'
 function Comment({ commentId, comments, users }) {
   const comment = comments.items[commentId]
 
+  const rate = () => {
+    const rates = comment.rates
+    const length = Object.keys(rates).length
+    if (!length) return 0
+    let result = Object.keys(rates).reduce((acc, key) => {
+      return acc + rates[key]
+    }, 0) / length
+    return result
+  }
+
   return (
     <div className="Comment">
       <div className="Comment__body">
@@ -18,7 +28,7 @@ function Comment({ commentId, comments, users }) {
         <Copyright author={users.items[comment.authorId].name} timestamp={comment.timestamp}/>
       </div>
       <Rate
-        defaultValue={2.5}
+        defaultValue={rate()}
         onChange={() => {}}
         style={{ fontSize: 10, marginTop: 14 }}
         allowHalf
