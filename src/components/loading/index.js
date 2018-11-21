d11498a1df456d9fd7e80111983f54bcca0c189c
Aleksandr Borkov <sbor767@yandex.ru>
2018-11-22 01:25:09 +0300

    Fix CSS for Loading component.

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import './style.css'
+
 
 export default function Loading() {
   return (
     <div className='Loading'>
-      <img src="/assets/icon.png" alt="logo" id="loader" />
+      <img src="/assets/react-icon.svg" alt="Loading..." className="Loading__img" />
     </div>
   )
 }
