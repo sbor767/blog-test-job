b0ac4355bb1d052a27d9d7ce52650ed2a60f71e5
Aleksandr Borkov <sbor767@yandex.ru>
2018-11-30 20:22:17 +0300

    Fix RoutesPosts router component to right path treatment when user logged out.

--- src/app/routes/posts/index.js
+++ src/app/routes/posts/index.js
@@ -1,32 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { PagesPost, PagesPostCreate } from '../../../components/pages'
+import PropTypes from 'prop-types'
 
 
-function RoutesPosts({ user }) {
+class RoutesPosts extends Component {
 
-  return (
-    <Switch>
-      <Route
-        path="/posts/create"
-        render={({ history }) => (
-          user.id ? (
-            <PagesPostCreate history={history} />
-          ) : (
-            <Redirect to="/" />
-          ))}
-      />
-      <Route
-        path="/posts/:id"
-        render={({ history, match }) => <PagesPost postId={+match.params.id} history={history} />}
-      />
-    </Switch>
-  )
+  static propTypes = {
+    isLoaded: PropTypes.bool,
+    user: PropTypes.object,
+  }
+
+  static defaultProps = {
+    isLoaded: false
+  }
+
+
+  render() {
+    const { isLoaded, user } = this.props
+
+    return (
+      <Switch>
+
+        <Route
+          path="/posts/create"
+          render={({ history }) => {
+            if (!isLoaded || user.id) {
+              return <PagesPostCreate history={history} />
+            } else {
+              return <Redirect to="/" />
+            }
+          }}
+        />
+
+        <Route
+          path="/posts/:id"
+          render={({history, match}) => <PagesPost postId={+match.params.id} history={history} />}
+        />
+
+      </Switch>
+    )
+  }
 }
 
+const mapStateToProps = state => ({
+  user: state.user,
+  isLoaded: !state.posts.isFetching
+    && !state.posts.didInvalidate
+    && !state.comments.isFetching
+    && !state.comments.didInvalidate
+    && !!state.users
+    && !!state.user
+})
 
-export default connect(state => ({
-  user: state.user
-}))(RoutesPosts)
+
+export default connect(mapStateToProps)(RoutesPosts)
